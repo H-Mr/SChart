78f7469edcf5581e9bc6a5a101f9c5e7f2edea10
hjw <487803149@qq.com>
2024-01-15 06:28:19 +0800

    Initial commit

---
+++ src/main/java/com/jw/schart/schart/SChartApplication.java
@@ -0,0 +1,13 @@
+package com.jw.schart.schart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SChartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SChartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jw/schart/schart/SChartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jw.schart.schart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SChartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
